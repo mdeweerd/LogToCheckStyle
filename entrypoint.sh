1004a1d7383463467ffc24dbb6ce406cfd974780
MDW <mdeweerd@users.noreply.github.com>
2024-01-16 02:53:54 +0100

    Add error to try action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -lxv
-[ "$3" = ""  ] && python /logToCs.py "$1" "$2" --root "$PWD"
-[ "$3" != "" ] && python /logToCs.py "$1" "$2" --root "$3"
+[ "$3" = ""  ] && python /logToCs.py "$1" "$2" --root "$PWD" && exit 0
+[ "$3" != "" ] && python /logToCs.py "$1" "$2" --root "$3"   && exit 0
--- logToCs.py
+++ logToCs.py
@@ -10,6 +10,10 @@ The log can then be used for generating annotations in a github action.
 Note: this script is very young and "quick and dirty".
       Patterns can be added to "PATTERNS" to match more messages.
 
+master
+dmmy
+xwindows
+
 # Examples
 
 Assumes that logToCs.py is available as .github/logToCs.py.
