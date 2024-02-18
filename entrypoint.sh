caf5bd1bb30f54e3283b997cbd6ffb997720962e
MDW <mdeweerd@users.noreply.github.com>
2024-02-18 16:00:29 +0100

    Fix path, add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 ANNOTATE=""
+echo "CMD: $0/$(realpath "$0") PWD: $PWD ARGS: ${@[*]}"
 [ "$3" = "true" ] && ANNOTATE="--github-annotate"
 [ "$3" = "false" ] && ANNOTATE="--no-github-annotate"
-[ "$3" = ""  ] && python /logToCs.py "$1" "$2" --root "$PWD" && exit 0
-[ "$3" != "" ] && python /logToCs.py "$1" "$2" --root "$3"   && exit 0
+[ "$3" = ""  ] && python ./logToCs.py "$1" "$2" --root "$PWD" && exit 0
+[ "$3" != "" ] && python ./logToCs.py "$1" "$2" --root "$3"   && exit 0
